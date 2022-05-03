9b34fcd93148d96b0eb67ef9d637fa84fe435ff6
Amir Motefaker <amotef@gmail.com>
2022-05-04 00:52:28 +0430

    Create Problem10.py

---
+++ Problem10.py
@@ -0,0 +1,29 @@
+# Code by amotef@gmail.com
+
+# projecteuler.net
+# Problem 10
+
+# Summation of primes
+
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+# Find the sum of all the primes below two million.
+
+import time
+start_time = time.time()   #Time at the start of program execution
+
+def is_Prime(n):
+    if n < 2: return "It is not a prime number."
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+sum = 0
+for i in range(2, 2000000):
+    if is_Prime(i):
+        sum += i
+
+print ("Sum of all the primes below two million =", sum)
+
+end_time = time.time()   #Time at the end of execution
+print ("Time of program execution:", (end_time - start_time))   # Time of program execution

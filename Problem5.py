9b18c41a7db8075a997bc9b7795a7169f2855b43
Amir Motefaker <amotef@gmail.com>
2022-05-06 03:35:53 +0430

    Update Problem5.py

--- Problem5.py
+++ Problem5.py
@@ -14,7 +14,7 @@
 
 
 import time
-start_time = time.time()   #Time at the start of program execution
+start_time = time.time()   # Time at the start of program execution
 
 from math import gcd  # gcd: Return the greatest common divisor of the specified integer arguments.
 from functools import reduce  # reduce: Apply function of two arguments cumulatively to the items of iterable, from left to right, so as to reduce the iterable to a single value.
@@ -25,5 +25,5 @@ N = int(input("The LCM for numbers 1 through "))
 N_reduce = reduce(LCM, range(N//2+1, N+1))
 print ("The LCM for numbers 1 through" ,N, "is:", N_reduce)
 
-end_time = time.time()   #Time at the end of execution
+end_time = time.time()   # Time at the end of execution
 print ("Time of program execution:", (end_time - start_time))   # Time of program execution

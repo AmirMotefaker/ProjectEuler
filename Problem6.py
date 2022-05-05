68b58a90a445d7d38f68e62aaa752d6a42e6cf96
Amir Motefaker <amotef@gmail.com>
2022-05-06 03:43:26 +0430

    Update Problem6.py

--- Problem6.py
+++ Problem6.py
@@ -4,6 +4,7 @@
 # Problem 6
 
 # Sum square difference
+
 # The sum of the squares of the first ten natural numbers is,
 # 1**2 + 2**2 + .... + 10**2 = 385
 # The square of the sum of the first ten natural numbers is,

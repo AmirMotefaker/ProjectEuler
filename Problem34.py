cd1c29cfe7089b0383a24def39c6ab60f17a3e78
Amir Motefaker <amotef@gmail.com>
2022-05-07 11:16:25 +0430

    Update Problem34.py

--- Problem34.py
+++ Problem34.py
@@ -13,37 +13,61 @@
 
 
 # Solution 1
-from math import factorial   #factorial function for calculating factorial
+# from math import factorial   #factorial function for calculating factorial
 
-import time
-start_time = time.time()   #Time at the start of program execution
+# import time
+# start_time = time.time()   #Time at the start of program execution
 
-#factorials of numbers from 0-9
-f = [factorial(0),
-factorial(1),
-factorial(2),
-factorial(3),
-factorial(4),
-factorial(5),
-factorial(6),
-factorial(7),
-factorial(8),
-factorial(9)]
+# #factorials of numbers from 0-9
+# f = [factorial(0),
+# factorial(1),
+# factorial(2),
+# factorial(3),
+# factorial(4),
+# factorial(5),
+# factorial(6),
+# factorial(7),
+# factorial(8),
+# factorial(9)]
 
-def factorial_digits(n):   #sum of factorial of the digits
-	s = 0
-	while n:
-		s += f[n%10]
-		n //= 10
-	return s
+# def factorial_digits(n):   #sum of factorial of the digits
+# 	s = 0
+# 	while n:
+# 		s += f[n%10]
+# 		n //= 10
+# 	return s
 
-solution = 0   #variable to save the value of solution
+# solution = 0   #variable to save the value of solution
 
-for i in range(10,1854721):   #for loop to loop till 1854721
-	if factorial_digits(i) == i:
-		solution += i
+# for i in range(10,1854721):   #for loop to loop till 1854721
+# 	if factorial_digits(i) == i:
+# 		solution += i
 
-print (solution)
+# print (solution)
 
-end_time = time.time()   #Time at the end of execution
-print ("Time of program execution:", (end_time - start_time))   # Time of program execution
+# end_time = time.time()   #Time at the end of execution
+# print ("Time of program execution:", (end_time - start_time))   # Time of program execution
+
+
+# Solution 2
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+
+def main():
+    grand_total = 0
+    for i in range(10, 100000):
+        str_i = str(i)
+        total = 0
+        for digit in str_i:
+            digit = int(digit)
+            total += factorial(digit)
+        if total == i:
+            grand_total += i
+            print(i, grand_total)
+
+
+main()

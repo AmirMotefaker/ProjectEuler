382d33f88bb313ac4d5a6ae00ace5a33b4f872bf
Amir Motefaker <amotef@gmail.com>
2022-05-11 11:57:11 +0430

    Update Problem43.py
    Add Time of program execution to this code

--- Problem43.py
+++ Problem43.py
@@ -6,7 +6,9 @@
 # Sub-string divisibility
 # Problem 43
 
-# The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.
+# The number, 1406357289, is a 0 to 9 pandigital number because it is
+# made up of each of the digits 0 to 9 in some order, but it also has
+# a rather interesting sub-string divisibility property.
 
 # Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
 
@@ -49,4 +51,3 @@ print (solution)
 
 end_time = time.time()   #Time at the end of execution
 print ("Time of program execution:", (end_time - start_time))   # Time of program execution
-

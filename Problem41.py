c8f75ba9080270a4645f59224b140d8734bea026
Amir Motefaker <amotef@gmail.com>
2022-05-11 09:49:03 +0430

    Update Problem41.py
    Added solution 3, that is fastest and fastest.

--- Problem41.py
+++ Problem41.py
@@ -88,32 +88,84 @@
 # try all permutations from 1 to 9 starting with 1 digit and going up,
 # filtering all prime numbers and, then, taking largest one.
 
-from itertools import permutations
-import time
+# from itertools import permutations
+# import time
 
-start_time = time.time()   #Time at the start of program execution
+# start_time = time.time()   #Time at the start of program execution
 
 
-def is_prime(n):
-    """Function to check if
-    the given number is prime"""
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            return False
-    return True
+# def is_prime(n):
+#     for i in range(2, int(n**0.5)+1):
+#         if n % i == 0:
+#             return False
+#     return True
 
 # permutations of numbers from 1-7
-p = permutations('1234567')
+# p = permutations('1234567')
 
 # for loop to loop from reverse order
 # from higher to lower
-for i in list(p)[::-1]:
-    if int(i[6]) % 2 != 0:
-        number = int(''.join(i))
-        if (number+1) % 6 == 0 or (number-1) % 6 == 0:
-            if is_prime(number):
-                print (number)
-                break
+# for i in list(p)[::-1]:
+#     if int(i[6]) % 2 != 0:
+#         number = int(''.join(i))
+#         if (number+1) % 6 == 0 or (number-1) % 6 == 0:
+#             if is_prime(number):
+#                 print (number)
+#                 break
+
+# end_time = time.time()   #Time at the end of execution
+# print ("Time of program execution:", (end_time - start_time))   # Time of program execution
+
+
+# Solution 3
+
+# This problem asks us to investigate the realm of pandigital numbers for the largest prime.
+# An obvious question is: what is the value of n in n-digit pandigital prime? 
+# We know it’s between 4 and 10 but that leaves a huge set of prime number candidates to search through.
+# So let’s eliminate some values of n by using the divisibility rule which states that an integer is
+# divisible by 3 whose sum of digits is divisible by 3 and therefore composite and not prime.
+
+# # 9+8+7+6+5+4+3+2+1+0 = 45, 45/3 = 15
+# 9+8+7+6+5+4+3+2+1 = 45, 45/3 = 15
+# 8+7+6+5+4+3+2+1 = 36, 36/3 = 12
+# 7+6+5+4+3+2+1 = 28, 28/3 = 9.333…
+# 6+5+4+3+2+1 = 21, 21/3 = 7
+# 5+4+3+2+1 = 15, 15/3 = 5
+# 4+3+2+1 = 10, 4/3 = 1.333…
+# 3+2+1 = 6, 6/3 = 2
+# 2+1 = 3 3/3 = 1
+
+# Only pandigital numbers of length 4 and 7 can form a prime number. 
+# The others cannot because any combination of their digits will sum to a number divisible by 3
+# as affirmed by the commutative law of addition. So that leaves us to search odd 4 and 7 digit
+# pandigital numbers for a prime number less than 4321 and 7654321 respectively.
+# We also know that the candidate must end with a 1, 3, or 7 to be considered as a possible prime number,
+# but this turned out to be of little consequence.
+# Since we want the largest pandigital prime we’ll start with 7 digit numbers.
+
+def is_pandigital(n, s=9): n=str(n); return len(n)==s and not '1234567890'[:s].strip(n)
+
+import time
+start_time = time.time()   #Time at the start of program execution
+
+def is_prime(n):
+    if n <= 1: return False
+    if n <= 3: return True
+    if n%2==0 or n%3 == 0: return False
+    r = int(n**0.5)
+    f = 5
+    while f <= r:
+        if n%f == 0 or n%(f+2) == 0: return False
+        f+= 6
+    return True
+
+
+n = 7654321
+
+while not(is_pandigital(n, 7) and is_prime(n)): n-= 2
+
+print ("The largest existing n-digit pandigital prime is", n)
 
 end_time = time.time()   #Time at the end of execution
 print ("Time of program execution:", (end_time - start_time))   # Time of program execution
+

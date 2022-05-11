d48383b0493198a47e20d0dd08b78fa69205d004
Amir Motefaker <amotef@gmail.com>
2022-05-11 09:33:01 +0430

    Update Problem41.py

--- Problem41.py
+++ Problem41.py
@@ -14,10 +14,41 @@
 # Solution 1
 # permutation method
 
+# a = '123456789', which when permuted will give all the 1-9 pandigital numbers.
+# If you don't want 1-9 pandigital and suppose say, you want only 1-7 pandigital number,
+#  then consider only a[:7].
+
+# flag = True
+
+# j = 0, this one is while loop iterator
+
+# While loop is executed because the value of flag is True at the instant.
+
+# p = permutations(a[:9]) = permutations('123456789')
+
+# As we know that the permutations will return all the numbers arranged from small to big.
+# As we wanted bigger prime number we will check from last to first.
+# I have used [::-1] to reverse the given list.
+
+# Take an element from the permutations and then check if the last element is not even.
+
+# Next condition uses the fact that all the prime numbers are of the form 6k+1 and 6k-1
+# but the vice versa is not true.
+
+# If the number passes the above test then check if it is prime.
+# If it is prime then this will be the largest pandigital prime number because we are 
+# coming in reverse order.
+
+# If any prime is not found in the present iteration then start checking permutations 
+# of '12345678', i.e.j = 8 = 9-1. At the end when the prime is found, 
+# print the prime number and then flag = False to stop the outer while loop and break
+# to stop the present for loop.
+
 from itertools import permutations  # permutations: Return successive r length permutations of elements in the iterable.
 import time
 
 start_time = time.time()   #Time at the start of program execution
+
 
 def is_prime(n):  # check if the given number is prime or not.
     for i in range(2, int(n**0.5)+1):

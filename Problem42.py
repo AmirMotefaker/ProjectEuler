15ee82f39e731a5d5c0cef196c5d564053f4fe1f
Amir Motefaker <amotef@gmail.com>
2022-05-11 10:16:07 +0430

    Update Problem42.py
    Add solution 2

--- Problem42.py
+++ Problem42.py
@@ -1,3 +1,5 @@
+# Code by amotef@gmail.com
+
 # projecteuler.net
 
 # Coded triangle numbers
@@ -17,8 +19,54 @@
 # how many are triangle words?
 
 # Solution 1
-from math import sqrt
-score = lambda word: sum(ord(c)-ord('A')+1 for c in word)
-is_tn = lambda t: (sqrt(1 + 8*t) - 1) / 2 % 1 == 0
-print ("Number of triangle words =", 
-    sum(is_tn(score(x[1:-1])) for x in open('Problem42_words.txt').read().split(',')))
+# from math import sqrt
+
+# import time
+# start_time = time.time()   #Time at the start of program execution
+
+# score = lambda word: sum(ord(c)-ord('A')+1 for c in word)
+# is_tn = lambda t: (sqrt(1 + 8*t) - 1) / 2 % 1 == 0
+# print ("Number of triangle words =", 
+#     sum(is_tn(score(x[1:-1])) for x in open('Problem42_words.txt').read().split(',')))
+
+# end_time = time.time()   #Time at the end of execution
+# print ("Time of program execution:", (end_time - start_time))   # Time of program execution
+
+
+
+# Solution 2
+import string
+import csv
+import math
+
+import time
+start_time = time.time()   #Time at the start of program execution
+
+
+def main():
+    letter_to_number_dict = dict(zip(string.ascii_uppercase, [ord(c) % 32 for c in string.ascii_uppercase]))
+    with open('Problem42_words.txt', 'r') as f:
+        reader = csv.reader(f)
+        words = list(reader)[0]
+    total = 0
+    for word in words:
+        score = 0
+        for letter in word:
+            score += letter_to_number_dict[letter]
+        if is_square(8*score+1):
+            total += 1
+    print(total)
+
+
+def is_square(integer):
+    root = math.sqrt(integer)
+    if int(root + 0.5) ** 2 == integer:
+        return True
+    else:
+        return False
+
+
+main()
+
+end_time = time.time()   #Time at the end of execution
+print ("Time of program execution:", (end_time - start_time))   # Time of program execution

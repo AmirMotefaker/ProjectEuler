809940f493a93ef8febc3a32d28201e6273fb275
Amir Motefaker <amotef@gmail.com>
2022-05-12 09:34:46 +0430

    Update Problem48.py
    Solution 1 has a simpler algorithm

--- Problem48.py
+++ Problem48.py
@@ -12,34 +12,34 @@
 
 # Solution 1
 
-# import time
-# start_time = time.time()   #Time at the start of program execution
+import time
+start_time = time.time()   #Time at the start of program execution
 
-# solution = 0
+solution = 0
 
-# for i in range(1, 1001):
-#     solution += i**i
+for i in range(1, 1001):
+    solution += i**i
 
-# print (str(solution)[-10:])   # printing the last 10 digits
+print (str(solution)[-10:])   # printing the last 10 digits
 
-# end_time = time.time()   #Time at the end of execution
-# print ("Time of program execution:", (end_time - start_time))   # Time of program execution
+end_time = time.time()   #Time at the end of execution
+print ("Time of program execution:", (end_time - start_time))   # Time of program execution
 
 
 
 # Solution 2
 
-import time
-start_time = time.time()   #Time at the start of program execution
+# import time
+# start_time = time.time()   #Time at the start of program execution
 
-def main():
-    total = 0
-    for i in range(1,1001):
-        total += i**i
-    print(str(total)[-10:])
+# def main():
+#     total = 0
+#     for i in range(1,1001):
+#         total += i**i
+#     print(str(total)[-10:])
 
 
-main()
+# main()
 
-end_time = time.time()   #Time at the end of execution
-print ("Time of program execution:", (end_time - start_time))   # Time of program execution
+# end_time = time.time()   #Time at the end of execution
+# print ("Time of program execution:", (end_time - start_time))   # Time of program execution

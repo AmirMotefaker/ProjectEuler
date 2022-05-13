1adabf704705d909bdc55ef789f5d0651285f537
Amir Motefaker <amotef@gmail.com>
2022-05-13 13:43:46 +0430

    Update Problem3.py

--- Problem3.py
+++ Problem3.py
@@ -1,9 +1,11 @@
 # Code by amotef@gmail.com
 
 # projecteuler.net
-# Problem 3
+# https://projecteuler.net/problem=3
 
 # Largest prime factor
+# Problem 3
+
 # The prime factors of 13195 are 5, 7, 13 and 29. Largest prime factor 13195 is 29.
 # What is the largest prime factor of the number 600851475143 ?
 

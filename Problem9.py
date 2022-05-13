48d5a95112d7ac318ee41cfb90a768a504ff608e
Amir Motefaker <amotef@gmail.com>
2022-05-13 13:52:16 +0430

    Update Problem9.py

--- Problem9.py
+++ Problem9.py
@@ -1,9 +1,10 @@
 # Code by amotef@gmail.com
 
 # projecteuler.net
-# Problem 9
+# https://projecteuler.net/problem=9
 
 # Special Pythagorean triplet
+# Problem 9
 
 # A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 # a**2 + b**2 = c**2

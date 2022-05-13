353eb1e7ecc033bf3442160c7ffc179a8dc51b9d
Amir Motefaker <amotef@gmail.com>
2022-05-13 14:02:34 +0430

    Update Problem13.py

--- Problem13.py
+++ Problem13.py
@@ -1,9 +1,11 @@
 # Code by amotef@gmail.com
 
 # projecteuler.net
-# Problem 13
+# https://projecteuler.net/problem=13
 
 # Large sum
+# Problem 13
+
 # Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 # 37107287533902102798797998220837590246510135740250
 # 46376937677490009712648124896970078050417018260538

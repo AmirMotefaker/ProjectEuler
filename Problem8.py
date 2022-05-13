bbc0e8623792e1f3943b9b26b101fcc64ac951ae
Amir Motefaker <amotef@gmail.com>
2022-05-13 13:49:40 +0430

    Update Problem8.py

--- Problem8.py
+++ Problem8.py
@@ -1,9 +1,10 @@
 # Code by amotef@gmail.com
 
 # projecteuler.net
-# Problem 8
+# https://projecteuler.net/problem=8
 
 # Largest product in a series
+# Problem 8
 
 # The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
 # 73167176531330624919225119674426574742355349194934

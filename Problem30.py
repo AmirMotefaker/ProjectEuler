399c46b4ffb416def45fc08ba65f462b21b65f70
Amir Motefaker <amotef@gmail.com>
2022-05-15 12:03:20 +0430

    Update Problem30.py
    Added answer.

--- Problem30.py
+++ Problem30.py
@@ -31,3 +31,7 @@ print (solution)
 
 end_time = time.time()   #Time at the end of execution
 print ("Time of program execution:", (end_time - start_time))   # Time of program execution
+
+
+
+### Answer:  443839

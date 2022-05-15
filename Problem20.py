6c4829b12d611e26a63a98145ab923a6712627f8
Amir Motefaker <amotef@gmail.com>
2022-05-15 11:36:36 +0430

    Update Problem20.py
    Added answer.

--- Problem20.py
+++ Problem20.py
@@ -38,3 +38,7 @@ main()
 # print("100! =", factorial(100))
 end_time = time.time()   #Time at the end of execution
 print ("Time of program execution:", (end_time - start_time))   # Time of program execution
+
+
+
+### Answer:  648

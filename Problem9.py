9c6a164a19e070f1c6a2cb27977551a5f270fee6
Amir Motefaker <amotef@gmail.com>
2022-05-15 11:15:08 +0430

    Update Problem9.py
    Added answer.

--- Problem9.py
+++ Problem9.py
@@ -34,3 +34,7 @@ for a in range(3, 1000):
 
 end_time = time.time()   #Time at the end of execution
 print ("Time of program execution:", (end_time - start_time))   # Time of program execution
+
+
+
+### Answer:  31875000

02a9e0d20472697bba51d8f1d4c5ad2940c49b88
Amir Motefaker <amotef@gmail.com>
2022-05-15 11:27:27 +0430

    Update Problem12.py
    Added answer.

--- Problem12.py
+++ Problem12.py
@@ -49,3 +49,7 @@ print (int(seed*(seed+1)/2))
 
 end_time = time.time()   #Time at the end of execution
 print ("Time of program execution:", (end_time - start_time))   # Time of program execution
+
+
+
+### Answer:  76576500

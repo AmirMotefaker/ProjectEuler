210c8dd9353045c50f5dc048fdc2649942063b11
Amir Motefaker <amotef@gmail.com>
2022-05-19 11:57:02 +0430

    Update Problem57.py
    Added solution 2.

--- Problem57.py
+++ Problem57.py
@@ -29,23 +29,49 @@
 # Solution 1
 # if p/q is is the first convergent then the next convergent can be written as (p+2q)/(p+q).
 
+# import time
+# start_time = time.time()   #Time at the start of program execution
+
+# p = 1
+# q = 1
+
+# counter = 0
+
+# for i in range(1000):
+#     a1 = p + 2*q
+#     b1 = p + q
+#     if len(str(a1)) > len(str(b1)):
+#         counter += 1
+#     p = a1
+#     q = b1
+
+# print (counter)
+
+# end_time = time.time()   #Time at the end of execution
+# print ("Time of program execution:", (end_time - start_time))   #Time of program execution
+
+
+
+# Solution 2
+# Expand the series as 2d + n for the numerator and d+n for the
+# denominator and count the number of times the length of the 
+# numerator exceeds the denominator as strings.
+
+# Using base 10 logs for the number of digits comparison between
+# the numerator and denominator yields a 100 times speed improvement
+# over comparing the lengths of them converted to strings.
+
 import time
 start_time = time.time()   #Time at the start of program execution
 
-p = 1
-q = 1
+from math import log10
+L, n, d, c = 1000, 3, 2, 0
 
-counter = 0
+for x in range(2, L+1):
+    n, d = n + 2*d, n + d
+    if int(log10(n)) > int(log10(d)): c += 1
 
-for i in range(1000):
-    a1 = p + 2*q
-    b1 = p + q
-    if len(str(a1)) > len(str(b1)):
-        counter += 1
-    p = a1
-    q = b1
-
-print (counter)
+print ("Numerator more digits than denominator =", c)
 
 end_time = time.time()   #Time at the end of execution
 print ("Time of program execution:", (end_time - start_time))   #Time of program execution

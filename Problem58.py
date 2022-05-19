f52bfbeb7c1453196dc13cdf84aca56a0f99ea81
Amir Motefaker <amotef@gmail.com>
2022-05-20 02:12:02 +0430

    Update Problem58.py
    Added solution 2 with sympy.

--- Problem58.py
+++ Problem58.py
@@ -23,7 +23,9 @@
 # a square spiral with side length 9 will be formed. 
 # If this process is continued, what is the side length of the square spiral for which the ratio of primes along both diagonals first falls below 10%?
 
+
 # Solution 1
+
 # Step-by-Step
 # Step 1: Write a function to test if the given number is prime.
 # Step 2: Generate all the diagonal numbers in the spiral, 
@@ -31,37 +33,67 @@
 # Step 3: If the length is less than 10%, break the loop and print the square root of the last number.
 #       Otherwise continue the next iteration with step1.
 
+# import time
+# start_time = time.time()   #Time at the start of program execution
+
+
+# def is_prime(n):
+#     for i in range(3, int(n**0.5)+1, 2):
+#         if n % i == 0:
+#             return False
+#     return True
+
+# i = 0
+
+# gap = 1
+
+# ratio = 1
+
+# primes = []
+
+# all_numbers = [1]
+
+# while ratio > 0.1:
+#     for j in range(4):
+#         i += gap
+#         present_number = 2*i + 1
+#         all_numbers.append(present_number)
+#         if is_prime(present_number):
+#             primes.append(2*i + 1)
+#     ratio = float(len(primes))/len(all_numbers)
+#     gap += 1
+
+# print ((2*i+1)**0.5)
+
+# end_time = time.time()   #Time at the end of execution
+# print ("Time of program execution:", (end_time - start_time))   #Time of program execution
+
+
+
+# Solution 2
+
 import time
 start_time = time.time()   #Time at the start of program execution
 
+from sympy import isprime
 
-def is_prime(n):
-    for i in range(3, int(n**0.5)+1, 2):
-        if n % i == 0:
-            return False
-    return True
+def main():
+    counter=0
+    n=2
+    while True:
+        diagelements=[4*n*n-10*n+7,4*n*n-8*n+5,4*n*n-6*n+3,(2*n-1)**2]
+        for x in diagelements:
+            if isprime(x):
+                counter+=1
+                #print(x)
+        #print(counter,4*n-3)
+        if (counter/(4*n-3))<0.1:
+            break
+        n+=1
+    print("The side length is: ",2*n-1)
 
-i = 0
-
-gap = 1
-
-ratio = 1
-
-primes = []
-
-all_numbers = [1]
-
-while ratio > 0.1:
-    for j in range(4):
-        i += gap
-        present_number = 2*i + 1
-        all_numbers.append(present_number)
-        if is_prime(present_number):
-            primes.append(2*i + 1)
-    ratio = float(len(primes))/len(all_numbers)
-    gap += 1
-
-print ((2*i+1)**0.5)
+if __name__ == '__main__':
+    main()
 
 end_time = time.time()   #Time at the end of execution
 print ("Time of program execution:", (end_time - start_time))   #Time of program execution

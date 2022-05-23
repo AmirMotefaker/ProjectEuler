c670dc7f6fc162250efe9cd895638ad67b62fac5
Amir Motefaker <amotef@gmail.com>
2022-05-23 17:09:03 +0430

    Update Problem2.py
    Added Modularity code

--- Problem2.py
+++ Problem2.py
@@ -13,51 +13,64 @@
 
 # Solution 1
 
-import time
-start_time = time.time()   #Time at the start of program execution
-
-def IsEven(n):
-    if n % 2 == 0:
-        return True
-    else:
-        return False
-
-first = 1
-second = 2
-
-sum = 0
-
-while (first < 4000000):
-    # print ("my first number is", first)
-    if IsEven(first):
-        # print ("oh it is even")
-        sum = sum + first
-        # print ("-----------> now the sum is", sum)
-    new = first + second
-    first = second
-    second = new
-print(sum)
-
-end_time = time.time()   #Time at the end of execution
-print ("Time of program execution:", (end_time - start_time))   # Time of program execution
-
-
-
-# Solution 2
-
 # import time
 # start_time = time.time()   #Time at the start of program execution
 
-# a, b = 1, 1
-# total = 0
-# while a <= 4000000:
-#     if a % 2 == 0:
-#         total += a
-#     a, b = b, a+b  # the real formula for Fibonacci sequence
-# print (total)
+# def IsEven(n):
+#     if n % 2 == 0:
+#         return True
+#     else:
+#         return False
+
+# first = 1
+# second = 2
+
+# sum = 0
+
+# while (first < 4000000):
+#     # print ("my first number is", first)
+#     if IsEven(first):
+#         # print ("oh it is even")
+#         sum = sum + first
+#         # print ("-----------> now the sum is", sum)
+#     new = first + second
+#     first = second
+#     second = new
+# print(sum)
 
 # end_time = time.time()   #Time at the end of execution
 # print ("Time of program execution:", (end_time - start_time))   # Time of program execution
 
 
+
+# Solution 2
+
+import time
+start_time = time.time()   #Time at the start of program execution
+
+a, b = 1, 1
+total = 0
+while a <= 4000000:
+    if a % 2 == 0:
+        total += a
+    a, b = b, a+b  # the real formula for Fibonacci sequence
+print (total)
+
+end_time = time.time()   #Time at the end of execution
+print ("Time of program execution:", (end_time - start_time))   # Time of program execution
+
+
 ### Answer:  4613732
+
+
+# def sum_even_fibonacci(limit)
+#     '''Return the sum of all even Fibonacci numbers
+#        not greater than LIMIT
+#     '''
+#     a, b = 1, 1
+#     total = 0
+#     while a <= limit:
+#         if a % 2 == 0:
+#             total += a
+#         a, b = b, a+b
+#     return total

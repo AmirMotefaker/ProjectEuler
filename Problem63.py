f0854076494d7ce0f3052156a50d5591039cb810
Amir Motefaker <amotef@gmail.com>
2022-05-23 16:44:30 +0430

    Update Problem63.py
    Added solution 2

--- Problem63.py
+++ Problem63.py
@@ -11,11 +11,31 @@
 
 # How many n-digit positive integers exist which are also an nth power?
 
-from math import log10
+# Solution 1
 
-s = 0
-for n in range(1, 10):
-    s += int(1 / (1 - log10(n)))
+# from math import log10
 
-print ("n-digit integers that are an nth power =", s)
+# s = 0
+# for n in range(1, 10):
+#     s += int(1 / (1 - log10(n)))
 
+# print ("n-digit integers that are an nth power =", s)
+
+
+
+# Solution 2
+
+counter = 0
+
+# for loop to loop from 1 to 9
+for i in range(1, 10):
+    power = 1
+    while True:
+        if power == len(str(i ** power)):
+            counter += 1
+        else:
+            break
+        power += 1
+
+# print the number of instances found
+print (counter)
